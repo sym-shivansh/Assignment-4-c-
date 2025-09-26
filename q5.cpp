b4e8bfca7dd9b9ecc34cefc4c85690588429e54b
Shivansh budakoti <cu25260080@coeruniversity.ac.in>
2025-09-26 16:49:53 +0530

    Create q5.cpp

---
+++ q5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a;
+    cout<<"enter a year"<<endl;
+       cin>>a;
+
+  if(a%4==0)
+  {
+    cout<<a<<"is a leap year"<<endl;
+  }
+  else{
+       if(a%100==0)
+       {
+         cout<<a<<"is not a leap year"<<endl;
+       }
+  else{
+    if(a%400==0)
+    {
+      cout<<a<<"is a leap year"<<endl;
+    }
+  else{
+    cout<<a<<"is not a leap year"<<endl;
+  }
+  }
+  }
+return 0;
+  
+}
