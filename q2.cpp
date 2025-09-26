14155fdbcd22c8ddef6d01de068f14fa11d750ef
Shivansh budakoti <cu25260080@coeruniversity.ac.in>
2025-09-26 09:37:55 +0530

    Create q2.cpp

---
+++ q2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a;
+cout<< "enter a number:"<< endl;
+  cin>>a;
+      if(a % 2==0)
+      {
+        cout<< a << "is even number"<<endl;
+      }
+  else{
+    cout<<a << "is odd number"<<endl;
+      }
+    
+return 0;
+
+
+
+  
+}
