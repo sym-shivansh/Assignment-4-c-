a9400a8e77487616f4e6fa8bdaf7ee5011d00bc7
Shivansh budakoti <cu25260080@coeruniversity.ac.in>
2025-09-26 09:27:42 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+     cout<< "enter a number";
+     cin>>a;
+if (a>0)
+{
+cout<< a << "is positive";
+}
+       else {
+if (a<0)
+{
+cout<< a << is "negetive";
+}
+        else {
+cout<< "number is zero";
+        }
+}
+return 0;
+}
+
+
+
+
+
+
+}
