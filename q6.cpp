94064dec2db47a7204f185b2ae1b50874443a4e7
Shivansh budakoti <cu25260080@coeruniversity.ac.in>
2025-09-26 17:18:26 +0530

    Create q6.cpp

---
+++ q6.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+    cout<<"enter your marks:"<<endl;
+     cin>>A;
+
+  if(a>=90 && a<=100)
+     {
+       cout<<"a grade"<<endl;
+     }
+      else{
+          if(a>=80 && a<=89)
+             {
+               cout<<"b grade"<<endl;
+             }
+              else{
+                  if(a>=70 && a<=79)
+                    {
+                      cout<<"c grade"<<endl;
+                    }
+               else{
+                 if (a>=60 && a<=69)
+                  {
+                    cout<<"d grade"<<endl;
+                  }
+       else{
+           if(a>=50 && a<=59)
+             {
+               cout<<"e grade"<<endl;
+             }
+     else{
+        if(a>50)
+          {
+            cout<<"fail"<<endl;
+          }
+     }
+     }
+     }
+     }
+      }
+return 0;
+}
+       
+
+                 
+
