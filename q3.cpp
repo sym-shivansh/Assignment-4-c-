e35d62f55a76a35ce3260384465e7dabaa1af739
Shivansh budakoti <cu25260080@coeruniversity.ac.in>
2025-09-26 09:56:14 +0530

    Create q3.cpp

---
+++ q3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout<< "enter numbers"<<endl;
+    cin>> a>>b;
+
+  if(a>b)
+  {
+    cout<< "a is greatest"<<endl;
+  }
+  else{
+    if(b>a)
+    {
+      cout<< "b is greatest"<<endl;
+    }
+  }
+return 0;
+}
+
+
+
+  
