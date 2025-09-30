a67a85740ddf8a17dbe2d21f512bbf5ed8d38da7
Shivansh budakoti <cu25260080@coeruniversity.ac.in>
2025-09-30 09:33:41 +0530

    Create q7.cpp

---
+++ q7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char ch;
+  cout << "enter a alphabet";
+  cin>>ch;
+  if (ch == 'a' || ch=='i'|| ch=='e'|| ch=='o'|| ch=='u')
+  {cout<< "vowel";}
+  else
+  { cout<< "consonant";}
+  return 0;
+
+  
+  
+}
