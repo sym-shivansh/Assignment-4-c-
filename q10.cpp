720a7a07f2edceb62b875e9d60d5a92ae1d5a6ac
Shivansh budakoti <cu25260080@coeruniversity.ac.in>
2025-09-30 09:57:15 +0530

    Create q10.cpp

---
+++ q10.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a ;
+cout<<"enter a month number";
+cin>>a;
+switch (a)
+{
+  case 1: cout<<"january";
+  break;
+   case 2: cout<<"febuary";
+  break;
+   case 3: cout<<"march";
+  break;
+   case 4: cout<<"april";
+  break;
+   case 5: cout<<"may";
+  break;
+   case 6: cout<<"june";
+  break;
+   case 7: cout<<"july";
+  break;
+   case 8: cout<<"august";
+  break;
+   case 9: cout<<"september";
+  break;
+   case 10: cout<<"october";
+  break;
+   case 11: cout<<"november";
+  break;
+   case 12: cout<<"december";
+  break;
+  default : cout<<"invalid number";
+}
+  return 0;
+}
+  
